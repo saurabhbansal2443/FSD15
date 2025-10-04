03f620c2909bf04bfd9a1a141c336562cf2e6676
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2025-10-04 18:39:53 +0530

    operators

---
+++ DataType.java
@@ -0,0 +1,15 @@
+public class DataType {
+    public static void main(String[] args) {
+        // DataType variableName = data
+
+        String name = "Saurabh";
+        int age = 25;
+        char gender = 'm';
+        boolean isWell = true;
+        double percentage = 93.5;
+
+        System.out.println(name);
+        System.out.println("User age is " + age);
+
+    }
+}
---
+++ Operators.java
@@ -0,0 +1,15 @@
+public class Operators {
+    public static void main(String[] args) {
+        // Airthemtic Opertors - + , - , * , / , %
+
+        int a = 10;
+        int b = 10;
+
+        // System.out.println(a % b);
+
+        // Comparision Operator -> <,>,<=,>=,==,!=
+
+        System.out.println(a != b);
+
+    }
+}
