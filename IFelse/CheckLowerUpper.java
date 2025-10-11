d2d42773e90ac047d40262b1b8deabeca8c0d904
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2025-10-11 20:58:49 +0530

    check upper lower case

---
+++ IFelse/CheckLowerUpper.java
@@ -0,0 +1,25 @@
+package IFelse;
+
+import java.util.*;
+
+public class CheckLowerUpper {
+    public static void main(String[] args) {
+        // Write a program to check whether
+        // a character is uppercase or lowercase
+        Scanner sc = new Scanner(System.in);
+
+        char ch = sc.next().charAt(0);
+
+        // 1 convert to ASCII
+        // ans = type casting
+
+        int asciiVal = (int) ch;
+
+        if (asciiVal >= 97 && asciiVal <= 122) {
+            System.out.println("lowercase");
+        } else if (asciiVal >= 65 && asciiVal <= 90) {
+            System.out.println("Upper case");
+        }
+
+    }
+}
